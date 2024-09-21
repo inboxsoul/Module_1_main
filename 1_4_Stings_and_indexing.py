69e028e1eaa31ea6715c83bbd83803fbbd9a80a9
inboxsoul <inboxsoul@gmail.com>
2024-09-22 00:27:02 +0300

    ok

--- 1_4_Stings_and_indexing.py
+++ 1_4_Stings_and_indexing.py
@@ -5,4 +5,3 @@ print(example[-1])
 print(example[5:])
 print(example[::-1])
 print(example[1::2])
-
---
+++ 1_7_Variables.py
@@ -0,0 +1,5 @@
+qty_completed_tasks = "12"
+spent_hrs = "1.5"
+course_name = "Python"
+time_per_task = (float(spent_hrs) / float(qty_completed_tasks))
+print("Курс:",course_name + ",", "всего задач:"+qty_completed_tasks+",", "затрачено часов:"+spent_hrs+",", "cреднее время выполнения", time_per_task, "часа.")
